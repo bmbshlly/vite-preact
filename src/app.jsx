8fb079e4303501ab07b82847f7750725fb63860f
bmbshlly <ritikagrawal0@gmail.com>
2023-09-18 02:54:13 +0530

    modal added

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,5 @@
 import { useState } from "preact/hooks";
-import Greeting from "./components/text";
-// import preactLogo from "./assets/preact.svg";
-// import viteLogo from "/vite.svg";
+import Greeting from "./components/greeting.jsx";
 //import "./app.css";
 
 export function App() {
@@ -10,14 +8,14 @@ export function App() {
   return (
     <>
       <script src="https://package.mayank-8ca.workers.dev" />
+      <script src="https://cdn.tailwindcss.com" />
       <div class="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
       </div>
-      <div>checking 123.</div>
       <Greeting name="test" />
-      <x-greeting name="Billy Jo"></x-greeting>
+      {/* <x-greeting name="Billy Jo"></x-greeting> */}
     </>
   );
 }
--- src/components/greeting.jsx
+++ src/components/greeting.jsx
@@ -1,8 +1,12 @@
 import register from "preact-custom-element";
 import "./taildwind.css";
+import ModalUnstyled from "./modal";
 
 const Greeting = ({ name = "World" }) => (
-  <p class="text-orange-500">Hello, {name}!</p>
+  <>
+    <p class="text-orange-500">Hello, {name}!</p>
+    <ModalUnstyled />
+  </>
 );
 
 register(Greeting, "x-greeting", ["name"], { shadow: false });
